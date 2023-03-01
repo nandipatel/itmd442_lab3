989b4877e109edf4664dd021b28a5168839a6416
Nandi Patel <npatel127@hawk.iit.edu>
2023-03-01 17:56:45 -0600

    added closing statements

--- connect.js
+++ connect.js
@@ -1,7 +1,16 @@
+// connecting and creating database
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(':memory:', err => {
-    if (err) {
+let d_base = new sqlite3.Database(':memory:', err => {
+    if(err) {
         return console.error(err.message);
     }
-    console.log('Connection to an in-memory database has been made.')
+    console.log('Connected to database.')
 });
+
+// closing database
+d_base.close((err) => {
+    if(err) {
+        return console.error(err.message);
+    }
+    console.log('Closing connection to the database.')
+})
