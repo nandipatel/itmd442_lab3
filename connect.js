d9f37b35a2244a3d76d46d43e56d91e14ede6177
Nandi Patel <npatel127@hawk.iit.edu>
2023-03-01 18:49:59 -0600

    was able to connect but not integrate

--- connect.js
+++ connect.js
@@ -1,10 +1,25 @@
 // connecting and creating database
 const sqlite3 = require('sqlite3');
 const express = require("express");
-var 
-let db = new sqlite3.Database(':memory:', err => {
+var con = express();
+
+const HTTP_PORT = 3500
+con.listen(HTTP_PORT, () => {
+    console.log("Server is listening on port " + HTTP_PORT);
+});
+
+const db = new sqlite3.Database('./database/contacts.db', (err) => {
     if(err) {
         return console.error(err.message);
+    } else {
+        db.run('CREATE TABLE IF NOT EXISTS contacts (\
+            id INTEGER PRIMARY KEY,\
+            f_name NVARCHAR(500),\
+            l_name NVARCHAR(500) "",\
+            email NVARCHAR(500) "",\
+            note NVARCHAR(500) "",\
+            date_time DATETIME\
+          );')
     }
     console.log('Connected to database.')
 });
